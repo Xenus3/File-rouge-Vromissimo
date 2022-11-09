35df533a5c3a814171d8127738b9600d77946ec5
Xenus3 <you@example.comk.aberbach@hotmail.comgit config --global user.email k.aberbach@hotmaik.comgit config --global user.name Xenus3$ git commit -m First>
2022-11-09 08:50:24 +0100

    crud 4/4

--- edit.php
+++ edit.php
@@ -6,6 +6,7 @@ include ('dbconfig.php');
 
 
 	if (isset($_GET['edit'])) {
+
 		$id = $_GET['edit'];
 		$update = true;
 		$sql = "SELECT * FROM voiture WHERE id_voiture=:id";
@@ -13,20 +14,52 @@ include ('dbconfig.php');
         $edit->execute( array(":id"=>$id));
 
 		if ($edit->rowCount() == 1 ) {
+
 			$n = $edit->fetch(PDO::FETCH_ASSOC);
+
             $photo = $n['photos_1'];
-			$marque = $n['marque'];
-			$modele = $n['modele'];
+			      $marque = $n['marque'];
+			      $modele = $n['modele'];
             $kilometrage = $n['kilometrage'];
             $carburant = $n['carburant'];
             $carroserie = $n['carroserie'];
             $prix = $n['prix'];
 
-            
+            if (isset($_POST['modifier'])) {
+
+              $id = $_POST['id'];
+              var_dump($id);
+              $marque = $_POST['marque'];
+              $modele = $_POST['modele'];
+              $kilometrage = $_POST['kilometrage'];
+              $carburant = $_POST['carburant'];
+              $carroserie = $_POST['carroserie'];
+              $prix = $_POST['prix'];
+              $photo = $_POST['file'];
+          
+              $sql = "UPDATE voiture SET marque=:marque, modele=:modele, kilometrage=:kilometrage, carburant=:carburant, carroserie=:carroserie, prix=:prix, photos_1=:photo WHERE id_voiture=:id";
+
+              $edit = $bdd->prepare($sql);
+
+              $edit->execute( array(":id"=>$id,
+                                    ":marque"=>$marque,
+                                    ":modele"=>$modele,
+                                    ":kilometrage"=>$kilometrage,
+                                    ":carburant"=>$carburant,
+                                    ":carroserie"=>$carroserie,
+                                    ":prix"=>$prix,
+                                    ":photo"=>$photo ));
+
+              $_SESSION['message'] = "Address updated!"; 
+
+              header('location: http://localhost/php/vroomissimo/index.php');
+          }
+          
 		}
+   
 	}
 
-
+  
 ?>
 
 
@@ -56,7 +89,7 @@ include ('dbconfig.php');
   <?php include("header.php"); ?>
     <div id="form_inserer">
         <h1 style="text-align:center; padding-bottom:60px">Modifiez votre Annonce</h1>
-        <form method="POST" id="form_2" action="upload.php" enctype="multipart/form-data">
+        <form method="POST" id="form_2" action="" enctype="multipart/form-data">
             <input type="hidden" name="id" value="<?= $id ?>">
             <label for="marque">Marque:</label>
             <input type="text" name="marque" required value="<?= $marque ?>">
@@ -78,7 +111,7 @@ include ('dbconfig.php');
             <br/>
             <label for="image">Selectionnez une image:</label>
             <input type="file" name="file" >
-            <?php if ($update == true): ?>
+            <?php if ($update === true): ?>
             <input type="submit" value="Modifier" name="modifier" style="background: #556B2F; width: 30%;"></input>
             <?php else: ?>
             <input type="submit" value="Soumettre" name="soumettre" style="width: 30%"></input>
@@ -89,26 +122,3 @@ include ('dbconfig.php');
 </html>
 
 
-<?php
-if (isset($_POST['modifier'])) {
-    $id = $_POST['id'];
-    var_dump($id);
-    $marque = $_POST['marque'];
-    $modele = $_POST['modele'];
-    $kilometrage = $_POST['kilometrage'];
-    $carburant = $_POST['carburant'];
-    $carroserie = $_POST['carroserie'];
-    $prix = $_POST['prix'];
-
-    $sql = "UPDATE voiture SET marque=:marque, modele=:modele, kilometrage=:kilometrage, carburant=:carburant, carroserie=:carroserie, prix=:prix WHERE id_voiture=:id";
-    $edit = $bdd->prepare($sql);
-    $edit->execute( array(":id"=>$id,
-                          ":marque"=>$marque,
-                          ":modele"=>$modele,
-                          ":kilometrage"=>$kilometrage,
-                          ":carburant"=>$carburant,
-                          ":carroserie"=>$carroserie,
-                          ":prix"=>$prix ));
-    $_SESSION['message'] = "Address updated!"; 
-    header('location: http://localhost/php/vroomissimo/index.php');
-}
